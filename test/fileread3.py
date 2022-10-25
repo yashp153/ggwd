51ac9fbcb2f5849ba9909ec5b30cde569ec59269
yashp153 <f20190023@goa.bits-pilani.ac.in>
2022-10-25 10:51:25 +0530

    edited fileread3

--- test/fileread3.py
+++ test/fileread3.py
@@ -5,11 +5,11 @@ import matplotlib.pyplot as plt
 from gwpy.timeseries import TimeSeries
 
 file = h5py.File("./output/default.hdf","r")
-print(file.keys())
-print(file['injection_parameters'].keys())
-print(file['injection_parameters']['h1_signal'])
+#print(file.keys())
+# print(file['injection_parameters'].keys())
+# print(file['injection_parameters']['h1_signal'])
 x = file['injection_parameters']['scale_factor']
-print(x[0])
+# print(x[0])
 # print(np.sum(np.array(x))/5000)
 # print(file['injection_samples']['h1_strain'])
 # print(file['injection_parameters']['h1_signal'][0])
@@ -21,19 +21,19 @@ def plotarr(arr):
   plt.plot(timeser)
   plt.show()
 
-strain = np.array(file['injection_samples']['h1_strain'])[0]
-noise = np.array(file['injection_samples']['h1_noise'])[0]
-real_signal = file['injection_parameters']['h1_signal'][0]
+strain = np.array(file['injection_samples']['h1_strain'])
+noise = np.array(file['injection_samples']['h1_noise'])
+real_signal = file['injection_parameters']['h1_signal']
 # # print(merged.shape)
 # # print(noise.shape)
 signal = strain - noise
 # # s1 = merged - noise
 # # #print(m1)
-plotarr(strain)
+# plotarr(strain)
 # plotarr(noise)
-plotarr(signal)
-plotarr(real_signal)
-plotarr(noise)
-# np.savetxt("bbh_merged.csv",merged, delimiter=" ")
-# np.savetxt("bbh_signal.csv",signal, delimiter=" ")
+# plotarr(signal)
+# plotarr(real_signal)
+# plotarr(noise)
+np.savetxt("bbh_strain.csv",strain, delimiter=" ")
+np.savetxt("bbh_signal.csv",signal, delimiter=" ")
 # plotarr(a)
